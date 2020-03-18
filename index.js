3c2d4cfe6f594a3ca6912e3429cc5b1d0f63d16a
DuckSoft <realducksoft@gmail.com>
2020-03-19 01:49:42 +0800

    using @actions/artifact instead
    now we can support multiple packages at a time!

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-const core = require('@actions/core');
-const path = require('path');
-const fs = require('fs');
-const process = require('process');
+const core = require("@actions/core");
+const artifact = require("@actions/artifact");
 
-// most @actions toolkit packages have async methods
+const path = require("path");
+const fs = require("fs");
+const process = require("process");
+
 async function run() {
   try {
-      const pkgs = fs.readdirSync(process.cwd(), { withFileTypes: true })
-        .filter(entry => entry.isDirectory())
-        .map(entry => 
-          fs.readdirSync(entry.name, { withFileTypes: true })
-            .filter(file => file.isFile() && file.name.indexOf(".pkg.tar") !== -1)
-            .map(file => ({
-              "pkgfile": file.name,
-              "pkgpath": path.join(entry.name, file.name)
-            }))
-        ).reduce((acc, x) => acc.concat(x), []);
-      
-      console.log(pkgs);
+    const pkgs = fs
+      .readdirSync(process.cwd(), { withFileTypes: true })
+      .filter(entry => entry.isDirectory())
+      .map(entry =>
+        fs
+          .readdirSync(entry.name, { withFileTypes: true })
+          .filter(file => file.isFile() && file.name.indexOf(".pkg.tar") !== -1)
+          .map(file => ({
+            pkgfile: file.name,
+            pkgpath: path.join(entry.name, file.name)
+          }))
+      )
+      .reduce((acc, x) => acc.concat(x), []);
 
-      if (pkgs.length > 1) {
-        core.setFailed("We do not support more than one package at a time!");
-        return;
-      } else if (pkgs.length === 0) {
-        core.setFailed("Cannot find a package in: " + process.cwd());
-        return;
-      }
+    console.log(pkgs);
 
-      core.setOutput("pkgfile", pkgs[0]['pkgfile']);
-      core.setOutput("pkgpath", pkgs[0]['pkgpath']);
+    if (pkgs.length === 0) {
+      core.setFailed(`Cannot find a package in: ${process.cwd()}`);
+      return;
+    }
+
+    core.info("Uploading artifacts...");
+
+    Promise.all(
+      pkgs.map(entry =>
+        artifact
+          .create()
+          .uploadArtifact(entry["pkgfile"], [entry["pkgpath"]], ".", {
+            continueOnError: false
+          })
+          .then(result => {
+            core.info(` - Uploaded: ${result.artifactName}`);
+          })
+      )
+    )
+      .then(() => {
+        core.info("All artifact uploaded.");
+      })
+      .catch(e => {
+        core.error(`Artifact upload error: ${e.message}`);
+      });
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
